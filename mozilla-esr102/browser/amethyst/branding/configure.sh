d9dcf05ec505cc730c72e91a639020bc6baf0171
hATrayflood <h.rayflood@gmail.com>
2022-09-20 15:28:48 +0900

    gecko102のwebviewer機能

---
+++ mozilla-esr102/browser/amethyst/branding/configure.sh
@@ -0,0 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+MOZ_APP_DISPLAYNAME=WebViewer
+MOZ_APP_VENDOR=Renesas
+MOZ_APP_UA_NAME=Firefox
+MOZ_DISTRIBUTION_ID=com.renesas
